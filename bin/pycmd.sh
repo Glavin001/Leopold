a006d68d8994101c1f0b777ad8dfe840266e3c4b
Glavin Wiechert <Glavin.Wiechert@gmail.com>
2013-10-17 02:21:37 -0300

    Further work fixing aftermath of #17.

--- bin/pycmd.sh
+++ bin/pycmd.sh
@@ -3,6 +3,27 @@
 # right now it uses moving of files, but we may want to communicate with the
 # python script another way.
 
+# Source: http://stackoverflow.com/a/1116890/2578205
+TARGET_FILE=$0
+cd `dirname $TARGET_FILE`
+TARGET_FILE=`basename $TARGET_FILE`
+# Iterate down a (possible) chain of symlinks
+while [ -L "$TARGET_FILE" ]
+do
+    TARGET_FILE=`readlink $TARGET_FILE`
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+done
+# Compute the canonicalized name by finding the physical path 
+# for the directory we're in and appending the target file.
+PHYS_DIR=`pwd -P`
+RESULT=$PHYS_DIR/$TARGET_FILE
+#echo $RESULT
+# Move up on level to installDir from bin/
+cd "$(dirname "$RESULT")" # installDir/bin/
+RESULT=`pwd -P`
+echo "$(dirname "$RESULT")"
+
 
 function error() {
     echo "Accepted modes:"
